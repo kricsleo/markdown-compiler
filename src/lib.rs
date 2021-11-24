13d404d0ed76d2f8f61996a06f3c7b759cb20d9d
许圣峰 <kricsleo@163.com>
2021-11-24 17:37:05 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -13,25 +13,14 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 extern {
     #[wasm_bindgen(js_namespace = console, js_name = log)]
     fn log(msg: &str);
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
 }
 
 #[wasm_bindgen]
 pub fn compile(text: &str) -> String {
-    log(&format!("received input: {:?}", text));
+    log(text);
     let compiler = Parser::new_ext(text.trim(), Options::all());
     let mut html = String::new();
     push_html(&mut html, compiler);
-    log(&format!("compiled output: {}", html));
+    log(html);
     return html;
-}
-
-#[wasm_bindgen]
-pub fn add(a: usize, b: usize) -> usize {
-    return a + b;
 }
